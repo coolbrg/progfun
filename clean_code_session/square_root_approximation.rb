fbf54f7e4bd8ae71a5ad3bf8127cdc9b2d7fc6a0
budhram gurung <budhram@vertisinfotech.com>
2014-06-18 15:58:03 +0530

    Updated square root approximation

--- clean_code_session/square_root_approximation.rb
+++ clean_code_session/square_root_approximation.rb
@@ -1,4 +1,26 @@
 # Calculating square root using Newton's Method:
+
+=begin
+# 1st Approach
+  def good_enough?(a, n, delta)
+    (a**2 - n).abs < delta
+  end
+
+  def next_guess(a, n)
+    (a + n/a) / 2.0
+  end
+
+  def cal_sqrt(n, delta = 0.001)
+    guess = n / 2.0
+    until good_enough?(guess, n, delta)
+      guess = next_guess(guess, n)
+    end
+    guess
+  end
+  cal_sqrt(16)
+=end
+
+# 2nd Approach
 def cal_sqrt(n, delta = 0.001)
   guess = n / 2.0
   until (guess**2 - n).abs < delta
@@ -7,4 +29,3 @@ def cal_sqrt(n, delta = 0.001)
   guess
 end
 cal_sqrt(16)
-
