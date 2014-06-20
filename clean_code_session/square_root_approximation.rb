fe3dd333d00f36d28bc05fc5b1a57b604d1311a3
budhram gurung <budhram@vertisinfotech.com>
2014-06-20 11:03:48 +0530

    Updated to final code

--- clean_code_session/square_root_approximation.rb
+++ clean_code_session/square_root_approximation.rb
@@ -1,31 +1,30 @@
-# Calculating square root using Newton's Method:
+# Calculating square root using Newton-Raphson's Method:
 
-=begin
 # 1st Approach
-  def good_enough?(a, n, delta)
-    (a**2 - n).abs < delta
-  end
-
-  def next_guess(a, n)
-    (a + n/a) / 2.0
-  end
-
-  def cal_sqrt(n, delta = 0.001)
-    guess = n / 2.0
-    until good_enough?(guess, n, delta)
-      guess = next_guess(guess, n)
-    end
-    guess
-  end
-  cal_sqrt(16)
-=end
+# def cal_sqrt(n, delta = 0.001)
+#   guess = n / 2.0
+#   until (guess**2 - n).abs < delta
+#     guess = (guess + n/guess) / 2.0
+#   end
+#   guess
+# end
+# cal_sqrt(16)
 
 # 2nd Approach
+def good_enough?(a, n, delta)
+  (a**2 - n).abs < delta
+end
+
+def next_guess(a, n)
+  (a + n / a.to_f) / 2.0
+end
+
+def iter(n, guess, delta)
+  return guess if good_enough?(guess, n, delta)
+  iter n, next_guess(guess, n), delta
+end
+
 def cal_sqrt(n, delta = 0.001)
-  guess = n / 2.0
-  until (guess**2 - n).abs < delta
-    guess = (guess + n/guess) / 2.0
-  end
-  guess
+  iter n, n / 2.0, delta
 end
 cal_sqrt(16)
