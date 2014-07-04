93c73a5f58c52682eed3b3b7bf81ece24bbbff67
Budhram Gurung <budhram.gurung01@gmail.com>
2014-07-04 17:23:38 +0530

    Updated miniSurvey lib

--- js_fun/bootbox.js/js/simple_survey_app.js
+++ js_fun/bootbox.js/js/simple_survey_app.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
   $('#start_survey').on('click', function () {
-    var survey_questions;
-
-    survey_questions = {
-      name: 'What is your name?',
-      age: 'How old are you (in number only)?',
-      color: 'What is your favourite color?',
-      song: 'Which kind of song you like? <br>(bollywood, classical, pop, rock)',
-      movie: 'What is your favourite movie?'
-    };
+    var survey_questions = [
+      { key: 'name', text: 'What is your name?' },
+      { key: 'age', text: 'How old are you (in number only)?' },
+      { key: 'color', text: 'What is your favourite color?' },
+      { key: 'song', text: 'Which kind of song you like? <br>(bollywood, classical, pop, rock)' },
+      { key: 'movie', text: 'What is your favourite movie?' },
+    ];
 
     var displaySurveyResults = function(responses) {
       var survey_template = _.template($('#survey_template').html());
@@ -17,6 +15,6 @@ $(document).ready(function () {
       $('#start_survey').addClass('hide');
     }
 
-    multiBootbox(survey_questions, displaySurveyResults);
+    miniSurvey(survey_questions, displaySurveyResults);
   });
 });
