e395895fb5918aa55f6da8431a3c1d42b1e3d395
Budhram Gurung <budhram.gurung01@gmail.com>
2015-04-08 18:40:30 +0530

    AngularJS - Real time demo: Added CSS and updated view

---
+++ js_fun/angularjs/real_time_demo/scripts/controllers/nav.js
@@ -0,0 +1,10 @@
+app.controller('NavCtrl', function ($scope, $location, Post) {
+  $scope.post = {url: 'http://', title: ''};
+
+  $scope.submitPost = function () {
+    Post.create($scope.post).then(function (ref) {
+      $location.path('/posts/' + ref.name());
+      $scope.post = {url: 'http://', title: ''};
+    });
+  };
+});
--- js_fun/angularjs/real_time_demo/scripts/controllers/posts.js
+++ js_fun/angularjs/real_time_demo/scripts/controllers/posts.js
@@ -1,14 +1,6 @@
 app.controller('PostsCtrl', function ($scope, $location, Post) {
   $scope.posts = Post.all;
 
-  $scope.post = {url: 'http://', title: ''};
-
-  $scope.submitPost = function () {
-    Post.create($scope.post).then(function (ref) {
-      $location.path('/posts/' + ref.name());
-    });
-  };
-
   $scope.deletePost = function (post) {
     Post.delete(post);
   };
