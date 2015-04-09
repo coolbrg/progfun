d8a402ebad3234415a4fdad23d5f141fba2349c5
Budhram Gurung <budhram.gurung01@gmail.com>
2015-04-09 12:13:34 +0530

    AngularJS - Real time demo: Handling registration and login errors

--- js_fun/angularjs/real_time_demo/scripts/controllers/auth.js
+++ js_fun/angularjs/real_time_demo/scripts/controllers/auth.js
@@ -1,12 +1,14 @@
-app.controller('AuthCtrl', function ($scope, $location, Auth) {
-  if (Auth.signedIn()) {
+app.controller('AuthCtrl', function ($scope, $location, Auth, user) {
+  if (user) {
     $location.path('/');
   }
 
   $scope.login = function () {
     Auth.login($scope.user).then(function () {
       $location.path('/')
-    })
+    }, function(error){
+      $scope.error = error.toString();
+    });
   };
 
   $scope.register = function () {
@@ -14,6 +16,8 @@ app.controller('AuthCtrl', function ($scope, $location, Auth) {
       return Auth.login($scope.user).then(function() {
         $location.path('/');
       });
+    }, function(error) {
+      $scope.error = error.toString();
     });
   };
 });
