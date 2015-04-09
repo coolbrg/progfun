c5c1eebc24d797f9568c6029739c240aee6759e0
Budhram Gurung <budhram.gurung01@gmail.com>
2015-04-09 14:30:08 +0530

    AngularJS - Real time demo: Creating and deleting comments for post

--- js_fun/angularjs/real_time_demo/scripts/services/post.js
+++ js_fun/angularjs/real_time_demo/scripts/services/post.js
@@ -16,6 +16,9 @@ app.factory('Post', function ($firebase, FIREBASE_URL) {
     },
     delete: function (post) {
       return posts.$remove(post);
+    },
+    comments: function(postId) {
+      return $firebase(ref.child('comments').child(postId)).$asArray();
     }
   };
 
