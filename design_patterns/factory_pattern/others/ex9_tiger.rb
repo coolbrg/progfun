018dd5bce3d6eabda37778ad291112f0e64c8257
Budhram Gurung <budhram.gurung01@gmail.com>
2015-06-14 22:30:34 +0530

    Design Patterns : Completed factory pathern example codes

---
+++ design_patterns/factory_pattern/others/ex9_tiger.rb
@@ -0,0 +1,17 @@
+class Tiger
+  def initialize(name)
+    @name = name
+  end
+
+  def eat
+    puts "Tiger #{@name} eats anything it wants."
+  end
+
+  def speak
+    puts "Tiger #{@name} growls."
+  end
+
+  def sleep
+    puts "Tiger #{@name} sleep anywhere it wants."
+  end
+end
---
+++ design_patterns/factory_pattern/others/ex9_tree.rb
@@ -0,0 +1,9 @@
+class Tree
+  def initialize(name)
+    @name = name
+  end
+
+  def grow
+    puts "The Tree #{@name} grow talls."
+  end
+end
